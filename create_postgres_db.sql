b804432862fb110d003ba7047126c691dce59b38
jn793 <35499832+jn793@users.noreply.github.com>
2022-08-27 18:58:17 -0400

    First SQL file to create postgresDB

---
+++ create_postgres_db.sql
@@ -0,0 +1,106 @@
+/*
+Fact Table: 
+-Match_ID
+-Player1ID
+-Player2ID
+-Character1ID
+-Character2ID
+-TimestampKey
+
+Dimensions:
+Player
+    -PlayerID
+    -PlayerName
+    -Region
+    -Rank
+    -RankProgress
+
+Match
+    -MatchID
+    -MatchStartTimestamp
+    -MatchEndTimestamp
+    -MatchStatus
+    -MatchWinner
+    -MatchLoser
+
+Character
+    -CharacterID
+    -CharacterName
+    -CharacterType
+
+DateTime
+    -TimestampKey
+    -Day
+    -Month
+    -Year
+    -CompetitiveSeason
+
+*/
+
+CREATE TABLE dim_player(
+    player_id int PRIMARY KEY,
+    username varchar(12) UNIQUE NOT NULL,
+    region varchar(16) NOT NULL,
+    rank varchar(16) NOT NULL,
+    rank_progress int NOT NULL
+);
+
+CREATE TABLE dim_match()
+    match_id int PRIMARY KEY,
+    match_start_epoch int NOT NULL,
+    match_end_epoch int,
+    match_status varchar(4),
+    match_winner_id(int),
+    match_loser_id(int)
+;
+
+CREATE TABLE dim_character(
+    character_id int PRIMARY KEY,
+    character_name varchar(16) UNIQUE NOT NULL,
+    character_type varchar(32)
+);
+
+CREATE TABLE dim_datetime(
+    epoch_timestamp int PRIMARY KEY,
+    dt_day int NOT NULL,
+    dt_month varchar(16) NOT NULL,
+    dt_year int NOT NULL,
+    competitive_season varchar(32) NOT NULL
+);
+
+CREATE TABLE fact_player_match (
+    match_id int NOT NULL,
+    player_id int NOT NULL,
+    character_id int NOT NULL,
+    timestamp_key int NOT NULL,
+    FOREIGN KEY (match_id) REFERENCES dim_match (match_id),
+    FOREIGN KEY (player_id) REFERENCES dim_player (player_id),
+    FOREIGN KEY (character_id) REFERENCES dim_character (character_id),
+    FOREIGN KEY (timestamp_key) REFERENCES dim_datetime (epoch_timestamp),
+    PRIMARY KEY(match_id,player_id,character_id,timestamp_key)
+);
+/*
+CREATE TABLE fact_player_match (
+    player_id int NOT NULL,
+    match_id int NOT NULL,
+    FOREIGN KEY player_id references dim_player (player_id)
+    PRIMARY KEY(player_id, match_id,)
+);
+
+CREATE TABLE dim_player(
+    player_id int PRIMARY KEY,
+    FOREIGN KEY (player_id) REFERENCES fact_player_match (player_id),
+    username varchar(12) UNIQUE NOT NULL,
+    region varchar(16) NOT NULL,
+    rank varchar(16) NOT NULL,
+    rank_progress int NOT NULL
+);
+
+CREATE TABLE fact_player_match (
+    match_id int NOT NULL,
+    player_id int NOT NULL,
+    character_id int NOT NULL,
+    timestamp_key int NOT NULL
+    PRIMARY KEY(match_id,player_id,character_id,timestamp_key)
+);*/
+
