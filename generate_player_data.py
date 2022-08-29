0a763d36c5c2ea85ba953a1e6bcf82563c85dc6a
jn793 <35499832+jn793@users.noreply.github.com>
2022-08-29 18:06:40 -0400

    Altered to create a parquet file instead as I wanted to learn how to work with that

--- generate_player_data.py
+++ generate_player_data.py
@@ -1,11 +1,13 @@
 import random
-import json
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
 
 def main():
     generate_player(10000)
 
 def generate_player(player_count):
-    player_dict={}
+    players=[]
 
     player_name_prefix=['Doc','Grumpy','Happy','Sleepy','Bashful','Sneezy','Dopey']
     prefix_len=len(player_name_prefix)
@@ -19,10 +21,8 @@ def generate_player(player_count):
     rank_len=len(player_rank)
 
     for x in range(0,player_count):
-        
-
+        player_row=[]
         player_id=random.randrange(0,1000000)
-        player_dict[player_id]={}
 
         player_name_num=random.randrange(1,1000)
 
@@ -31,13 +31,14 @@ def generate_player(player_count):
             player_name_suffix[random.randrange(0,suffix_len)]+
             str(player_name_num))
         
-        player_dict[player_id]['username']=player_name
-        player_dict[player_id]['region']=player_region[random.randrange(0,region_len)]
-        player_dict[player_id]['rank']=player_rank[random.randrange(0,rank_len)]
-        player_dict[player_id]['rankprogress']=random.randrange(0,100)
-
-    with open('./data/generated_player_data/player_data.json','w') as fp:
-        json.dump(player_dict,fp,indent=4)
-
+        player_row=[player_id,player_name,player_region[random.randrange(0,region_len)],
+            player_rank[random.randrange(0,rank_len)],random.randrange(0,100)]
+        players.append(player_row)
+    
+    df=pd.DataFrame(players,columns=['id','username','region','rank','rank_progress'])
+    df=df.drop_duplicates(subset=['id'])
+    
+    table=pa.Table.from_pandas(df)
+    pq.write_table(table,'./data/generated_players.parquet')
 
 main()
